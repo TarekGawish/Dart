201b27c7d92aaa0f11125a3ccb79cbbab5d0f68f
tarek gawish <tarek29gawesh55@gmail.com>
2023-09-11 08:46:04 +0300

    dataTypes

--- variables.dart
+++ variables.dart
@@ -19,6 +19,9 @@ void main() {
   x = x + 1;
   print(x);
 
+  //string datatype
+  String name = "tarek";
+  print(name);
   //concatination
 
   String firstName = "Tarek";
@@ -28,6 +31,34 @@ void main() {
   print(firstName + " " + lastName);
 
   print('$firstName $lastName'); // The correct way
-  print('$firstName $lastName I am  ${x+y} years'); // makeing operations
-  
+  print('$firstName $lastName I am  ${x + y} years'); // makeing operations
+
+  //bool datatype
+  bool t = true;
+  bool f = false;
+
+  int num1 = 3;
+  int num2 = 2;
+  bool num = num1 == num2; // does num1 == num2 ?
+  print(num);
+
+  //list datatypes
+
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+
+  print(numbers);
+  print(numbers[2]); //index
+
+  print("add numbers ${numbers[3] + numbers[5]}");
+
+  // map datatype
+
+  Map<String, String> info = 
+  {
+    "name": "tarek",
+    "age": "21",
+    "address": "egypt-newDamitta"
+  };
+
+  print(info["address"]);
 }
