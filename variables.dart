b37991682112af5ecdb7dfd62f6e2eb5ba6aabd9
tarek gawish <tarek29gawesh55@gmail.com>
2023-09-11 06:57:39 +0300

    yes

--- variables.dart
+++ variables.dart
@@ -1,7 +1,17 @@
-void main()
+void main() 
 {
+  //int
   int firstNum = 50;
   int secNum = 30;
   int result = firstNum + secNum;
   print(result);
+
+  // initialization vs decleration
+
+  int x = 5; // initialization
+
+  int y; //Decleration
+  y = 6; 
+  
+
 }
