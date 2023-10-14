ce2f69f32ff695b29647027c90b8250020601e51
tarek gawish <tarek29gawesh55@gmail.com>
2023-10-14 08:17:11 +0300

    yes

--- 6-class and objects.dart
+++ 6-class and objects.dart
@@ -1,32 +1,6 @@
-import 'dart:ffi';
+void mian() {
+  print("hi");
 
-void main() {
-  Students student1 = Students("tarek", 21, "sammer");
-
-  print(
-      "the first student name is ${student1.name} and he is ${student1.age} years old and he is enroll in ${student1.semester} semester in ${student1.schoole} schoole.");
-
-  Students student2 = Students("osama", 23, "winter");
-
-  print("name is ${student2.name} age is ${student2.age} semester is ${student2.semester}");
-
-  student1.study();
-}
-
-class Students {
-  var name;
-  var age;
-  var semester;
-  var schoole;
-
-//parametric constractor
-  Students(var name, var age, var semester) {
-    this.name = name;
-    this.age = age;
-    this.semester = semester;
-  }
-
-  void study() {
-    print("study");
-  }
+  int x = 20;
+  print(x);
 }
